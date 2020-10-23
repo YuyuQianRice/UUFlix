9a14c0c7b6a94fe1a88992b5fededce434eabb7c
YuyuQianRice <Yuyu.Qian.Rice@gmail.com>
2020-10-22 22:41:03 -0500

    Initial Commit

---
+++ UUFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UUFlix
+//
+//  Created by Yuyu Qian on 10/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
